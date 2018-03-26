aee6d4c744ee0d12adf3a4371d2c2d045e31454f
Michal Sikora <michalsikora90@gmail.com>
2018-03-26 21:44:33 +0200

    Add display mode selector

---
+++ lib/DisplayMode/DisplayModeSelector.h
@@ -0,0 +1,27 @@
+#ifndef __INC_LEDBOW_DISPLAYMODESELECTOR_H
+#define __INC_LEDBOW_DISPLAYMODESELECTOR_H
+
+#include <DisplayMode.h>
+#include <DisplayModeProcessor.h>
+#include <Arduino.h>
+
+class DisplayModeSelector {
+public:
+  DisplayModeSelector() {}
+
+  void addProcessor(DisplayMode mode, DisplayModeProcessor* processor) {
+    int index = static_cast<int>(mode);
+    processors[index] = processor;
+  }
+
+  DisplayModeProcessor* selectProcessor(DisplayMode mode) {
+    int index = static_cast<int>(mode);
+    Serial.println(index);
+    return processors[index];
+  }
+
+private:
+  DisplayModeProcessor* processors[FINAL_DISPLAY_MODE_ENTRY];
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <DisplayModeIsr.h>
+#include <DisplayModeSelector.h>
 #include <SingleColorProcessor.h>
 
 #define BUTTON_PIN 2
@@ -6,13 +7,9 @@
 #define LED_PIN 6
 #define NUM_LEDS 30
 
-DisplayModeIsr modeIsr = DisplayModeIsr();
-SingleColorProcessor blackProcessor = SingleColorProcessor(NUM_LEDS, CHSV(0, 0, 0));
-SingleColorProcessor redProcessor = SingleColorProcessor(NUM_LEDS, CHSV(0, 255, 255));
-SingleColorProcessor greenProcessor = SingleColorProcessor(NUM_LEDS, CHSV(85, 255, 255));
-SingleColorProcessor blueProcessor = SingleColorProcessor(NUM_LEDS, CHSV(170, 255, 255));
-
 CRGB leds[NUM_LEDS];
+DisplayModeIsr modeIsr = DisplayModeIsr();
+DisplayModeSelector modeSelector = DisplayModeSelector();
 
 void onInterrupt() {
   modeIsr.onInterrupt(millis());
@@ -22,27 +19,16 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), onInterrupt, RISING);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  modeSelector.addProcessor(NONE, new SingleColorProcessor(NUM_LEDS, CHSV(0, 0, 0)));
+  modeSelector.addProcessor(RED, new SingleColorProcessor(NUM_LEDS, CHSV(0, 255, 255)));
+  modeSelector.addProcessor(GREEN, new SingleColorProcessor(NUM_LEDS, CHSV(85, 255, 255)));
+  modeSelector.addProcessor(BLUE, new SingleColorProcessor(NUM_LEDS, CHSV(170, 255, 255)));
 }
 
 void loop() {
-  unsigned long sleepTime = 0;
-  switch (modeIsr.currentMode()) {
-    case NONE:
-    sleepTime = blackProcessor.applyProcessor(leds);
-    break;
-
-    case RED:
-    sleepTime = redProcessor.applyProcessor(leds);
-    break;
-
-    case GREEN:
-    sleepTime = greenProcessor.applyProcessor(leds);
-    break;
-
-    case BLUE:
-    sleepTime = blueProcessor.applyProcessor(leds);
-    break;
-  }
+  DisplayMode mode = modeIsr.currentMode();
+  DisplayModeProcessor* processor = modeSelector.selectProcessor(mode);
+  unsigned long sleepTime = processor->applyProcessor(leds);
   FastLED.show();
   delay(sleepTime);
 }
